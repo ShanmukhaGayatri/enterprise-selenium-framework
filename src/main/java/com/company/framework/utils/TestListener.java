2be5c85e481a8fa1085a1f68e07dfe6694457627
shanmukhagayatri <shanmukhagayatri0910@gmail.com>
2026-01-12 18:27:04 +0530

    Fix TestListener screenshot capture method

--- src/main/java/com/company/framework/utils/TestListener.java
+++ src/main/java/com/company/framework/utils/TestListener.java
@@ -1,17 +1,27 @@
 package com.company.framework.utils;
 
+import com.company.framework.driver.DriverManager;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
 
     @Override
-    public void onTestSuccess(ITestResult result) {
-        ScreenshotUtils.captureScreenshot(result.getName() + "_PASS");
+    public void onTestFailure(ITestResult result) {
+        WebDriver driver = DriverManager.getDriver();
+
+        if (driver != null) {
+            ScreenshotUtils.capture(driver, result.getName());
+        }
     }
 
     @Override
-    public void onTestFailure(ITestResult result) {
-        ScreenshotUtils.captureScreenshot(result.getName() + "_FAIL");
+    public void onTestSuccess(ITestResult result) {
+        WebDriver driver = DriverManager.getDriver();
+
+        if (driver != null) {
+            ScreenshotUtils.capture(driver, result.getName());
+        }
     }
 }
